abd137921b92c4a79b50a9504867818197558562
ChinCobre <paluoctav2@gmail.com>
2026-09-21 16:09:34 +0200

    Añado cositas sabrosonas

---
+++ ProyectoFINAL/src/PARTE1/ConexionDB.java
@@ -0,0 +1,21 @@
+package PARTE1;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class ConexionDB {
+	public static void main(String[] args) {
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/libros?user=root&password=izquierdo");
+
+		} catch (SQLException ex) {
+		System.out.println("SQLException: " + ex.getMessage());
+		System.out.println("SQLState: " + ex.getSQLState());
+		System.out.println("VendorError: " + ex.getErrorCode());
+		}  
+
+	}		
+	}
