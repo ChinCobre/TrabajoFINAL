339ba2cf5c7be77ec38637b3cdf5958835273852
ChinCobre <paluoctav2@gmail.com>
2026-09-21 16:46:29 +0200

    ConexionDB hecha

--- ProyectoFINAL/src/PARTE1/ConexionDB.java
+++ ProyectoFINAL/src/PARTE1/ConexionDB.java
@@ -4,13 +4,22 @@ package PARTE1;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import io.github.cdimascio.dotenv.Dotenv;
 public class ConexionDB {
 	public static void main(String[] args) {
 		Connection conn = null;
 
 		try {
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/libros?user=root&password=izquierdo");
-
+			Dotenv dotenv =  Dotenv.load();
+			String dbhost = dotenv.get("DB_HOST");
+			String dbport = dotenv.get("DB_PORT");
+			String dbdatabase = dotenv.get("DB_DATABASE");
+			String dbusername = dotenv.get("DB_USERNAME");
+			String dbpassword = dotenv.get("DB_PASSWORD");
+			String url = "jdbc:mysql://" + dbhost + ":" + dbport + "/" + dbdatabase;
+			conn = DriverManager.getConnection(url, dbusername, dbpassword);
+			
 		} catch (SQLException ex) {
 		System.out.println("SQLException: " + ex.getMessage());
 		System.out.println("SQLState: " + ex.getSQLState());
